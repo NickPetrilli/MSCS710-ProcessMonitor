62d6ae5a8566b88ca24e42b43553e95168913a4c
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-01-30 17:09:24 -0500

    Project creation

---
+++ ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/ProcessMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.process_monitor.processmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessMonitorApplication.class, args);
+	}
+
+}
