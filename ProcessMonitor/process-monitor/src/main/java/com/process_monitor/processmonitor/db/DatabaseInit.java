541ba78eba629d7d6997e58fe53a893ef0d3b7de
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-02-16 16:06:32 -0500

    Database table changes, closes #2 #7

--- ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/db/DatabaseInit.java
+++ ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/db/DatabaseInit.java
@@ -5,13 +5,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * Class for initializing the database tables
+ */
 public class DatabaseInit {
 
     // Database URL
-    private static final String URL = "jdbc:sqlite:ProcessMonitor.db";
+    private final String URL = "jdbc:sqlite:ProcessMonitor.db";
 
-    // Create table SQL statement
-    private static final String CREATE_PROCESS_TABLE_SQL = "CREATE TABLE IF NOT EXISTS process ("
+    // Create table SQL statements
+    private final String CREATE_PROCESS_TABLE_SQL = "CREATE TABLE IF NOT EXISTS process ("
             + "process_id INTEGER,"
             + "timestamp TEXT,"
             + "name TEXT,"
@@ -23,28 +26,32 @@ public class DatabaseInit {
             + "PRIMARY KEY (process_id, timestamp)" 
             + ")";
 
-    private static final String CREATE_CPU_TABLE_SQL = "CREATE TABLE IF NOT EXISTS cpu ("
+    private final String CREATE_CPU_TABLE_SQL = "CREATE TABLE IF NOT EXISTS cpu ("
             + "timestamp TEXT PRIMARY KEY,"
             + "name TEXT,"
-            + "speed REAL,"
-            + "maxSpeed REAL,"
+            + "speed INTEGER,"
+            + "maxSpeed INTEGER,"
             + "cores INTEGER,"
             + "processes INTEGER,"
             + "threads INTEGER,"
             + "utilization REAL"
             + ")";    
 
-    private static final String CREATE_MEMORY_TABLE_SQL = "CREATE TABLE IF NOT EXISTS memory ("
+    private final String CREATE_MEMORY_TABLE_SQL = "CREATE TABLE IF NOT EXISTS memory ("
             + "timestamp TEXT PRIMARY KEY,"
-            + "totalMemory REAL,"
-            + "usedMemory REAL,"
+            + "totalMemory INTEGER,"
+            + "availableMemory INTEGER,"
+            + "usedMemory INTEGER,"
             + "utilization REAL"
             + ")";
             
-    private static final String CREATE_DISK_TABLE_SQL = "CREATE TABLE IF NOT EXISTS disk ("
+    private final String CREATE_DISK_TABLE_SQL = "CREATE TABLE IF NOT EXISTS disk ("
             + "timestamp TEXT PRIMARY KEY,"
             + "name TEXT,"
             + "model TEXT,"
+            + "swapTotal INTEGER,"
+            + "swapUsed INTEGER,"
+            + "swapUtilization REAL,"
             + "totalReadBytes REAL,"
             + "totalWriteBytes REAL,"
             + "transferTime REAL"
@@ -86,26 +93,8 @@ public class DatabaseInit {
         }
     }
 
-    public void insertStatement() {
-        try {
-            connection = DriverManager.getConnection(URL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        String insertCpu = "INSERT INTO cpu VALUES (1, 'test', 100, 200)";
-        try {
-            preparedStatement = connection.prepareStatement(insertCpu);
-            preparedStatement.executeUpdate();
-            System.out.println("Insertion completed");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-    }
     public static void main(String[] args) {
         DatabaseInit db = new DatabaseInit();
         db.createTables();
-        //db.insertStatement();
-
     }
 }
