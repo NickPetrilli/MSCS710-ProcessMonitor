e88e4727a3861d9f385eaa2db41cfe5351c7dfa0
Alec Lehmphul <61329825+aleclehmphul@users.noreply.github.com>
2024-02-20 17:59:20 -0500

    Add the REST API controller and service classes for process.  #11
    API endpoints return process data from the database as JSON.

--- ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/api/process/ProcessController.java
+++ ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/api/process/ProcessController.java
@@ -1,5 +1,6 @@
 package com.process_monitor.processmonitor.api.process;
 
+import com.process_monitor.processmonitor.api.process.model.Process;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller for handling requests and responses for Process.
+ */
 @RestController
 @RequestMapping(path = "api/v1/process")
 public class ProcessController {
@@ -19,14 +23,23 @@ public class ProcessController {
         this.processService = service;
     }
 
+    /**
+     * Gets list of most recent processes stored in database.
+     * @return List of most recent processes (in JSON)
+     */
     @GetMapping
     public List<Process> listProcesses() {
         return processService.getProcesses();
     }
 
-    @GetMapping("{id}")
-    public Process getProcessById(@PathVariable("id") Integer id) {
-        return processService.getProcess(id);
+    /**
+     * Gets process data with specified name.
+     * @param name Process name (ex: chrome)
+     * @return List of process data
+     */
+    @GetMapping("{name}")
+    public List<Process> getProcessHistoryByName(@PathVariable("name") String name) {
+        return processService.getProcessByName(name);
     }
 
 }
--- ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/api/process/ProcessService.java
+++ ProcessMonitor/process-monitor/src/main/java/com/process_monitor/processmonitor/api/process/ProcessService.java
@@ -1,16 +1,129 @@
 package com.process_monitor.processmonitor.api.process;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import com.process_monitor.processmonitor.api.process.model.Process;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Service class for responding to Process requests.
+ */
 @Service
 public class ProcessService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProcessService.class);
+
+    // Database URL
+    private final String URL = "jdbc:sqlite:ProcessMonitor.db";
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+    /**
+     * Retrieves list of all recent processes.
+     * @return List of processes
+     */
     public List<Process> getProcesses() {
-        return null;
+        List<Process> processList = new ArrayList<>();
+
+        try {
+            connection = DriverManager.getConnection(URL);
+
+            // Create select query - get all attributes for the latest timestamp
+            String sql = """
+                    SELECT *
+                    FROM process
+                    WHERE timestamp = (
+                        SELECT MAX(timestamp)
+                        FROM process
+                    )
+                    """;
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                processList.add(new Process(
+                        resultSet.getInt("process_id"),
+                        resultSet.getString("timestamp"),
+                        resultSet.getString("name"),
+                        resultSet.getString("status"),
+                        resultSet.getDouble("cpuPercentage"),
+                        resultSet.getLong("memUsageBytes"),
+                        resultSet.getDouble("memPercentage"),
+                        resultSet.getDouble("diskSpeed"),
+                        resultSet.getDouble("diskPercentage")
+                ));
+            }
+
+        } catch (SQLException e) {
+            logger.error("Error selecting CPU data.");
+            e.printStackTrace();
+        } finally {
+            // Close resources
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return processList.isEmpty() ? null : processList;
     }
 
-    public Process getProcess(Integer id) {
-        return null;
+    /**
+     * Gets a all process entries stored in database with specific name.
+     * @param name Name of process (ex: chrome)
+     * @return List of processes with specified name
+     */
+    public List<Process> getProcessByName(String name) {
+        List<Process> processList = new ArrayList<>();
+
+        try {
+            connection = DriverManager.getConnection(URL);
+
+            // Create select query - get all process information via name
+            String sql = "SELECT * FROM process WHERE name = \"" + name + "\""
+                    + " ORDER BY timestamp";
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                processList.add(new Process(
+                        resultSet.getInt("process_id"),
+                        resultSet.getString("timestamp"),
+                        resultSet.getString("name"),
+                        resultSet.getString("status"),
+                        resultSet.getDouble("cpuPercentage"),
+                        resultSet.getLong("memUsageBytes"),
+                        resultSet.getDouble("memPercentage"),
+                        resultSet.getDouble("diskSpeed"),
+                        resultSet.getDouble("diskPercentage")
+                ));
+            }
+
+        } catch (SQLException e) {
+            logger.error("Error selecting CPU data.");
+            e.printStackTrace();
+        } finally {
+            // Close resources
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return processList.isEmpty() ? null : processList;
     }
 }
