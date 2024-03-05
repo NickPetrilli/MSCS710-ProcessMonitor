58d856a96aab62875fbde608c7d92ed0ca17a869
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-03-05 17:52:10 -0500

    Configured router-dom to route to CpuDetail page
    -Added Detail folder for Detail views
    - Created Home page and moved logic from App.js
    - CPU Glance in Home page links to separate CPU Detail page

---
+++ sysmon-plus/src/Pages/Detail/DiskDetail.js
@@ -0,0 +1,13 @@
+// diskSection.js
+import React from 'react';
+
+const DiskDetail = () => {
+  return (
+    <div className="DiskDetail">
+      <h2>Disk</h2>
+      <p>**ENTER DISK NAME HERE**</p>
+    </div>
+  );
+};
+
+export default DiskDetail;
---
+++ sysmon-plus/src/Pages/Detail/MemoryDetail.js
@@ -0,0 +1,13 @@
+// memSection.js
+import React from 'react';
+
+const MemoryDetail = () => {
+  return (
+    <div className="MemoryDetail">
+      <h2>Memory</h2>
+      <p>**ENTER MEMORY NAME HERE**</p>
+    </div>
+  );
+};
+
+export default MemoryDetail;
