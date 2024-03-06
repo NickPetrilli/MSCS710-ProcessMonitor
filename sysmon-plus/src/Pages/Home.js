48195bcefdc793df9f363622f668ce35b40f6397
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-03-06 12:43:12 -0500

    Added router link to DiskDetail page

--- sysmon-plus/src/Pages/Home.js
+++ sysmon-plus/src/Pages/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import logo from '../Images/DoTTed Team Logo.png';
-import { Route, Routes} from 'react-router-dom';
 
 import CpuSection from './Glance/CpuGlance';
 import MemSection from './Glance/MemoryGlance';
 import DiskSection from './Glance/DiskGlance';
 import Processes from './Processes';
-import CpuDetail from './Detail/CpuDetail';
-import MemoryDetail from './Detail/MemoryDetail';
-import DiskDetail from './Detail/DiskDetail';
+
 
 const Home = () => {
     return (
@@ -19,13 +16,6 @@ const Home = () => {
             <h1 className="App-title-header"> SysMon+ </h1>
           </div>
         </header>
-  
-        <Routes>
-          {/* Define routes */}
-          <Route path = "/cpu-detail" element={<CpuDetail />} />
-          <Route path = "/memory-detail" element={<MemoryDetail />} />
-          <Route path = "/disk-detail" element={<DiskDetail />} />
-        </Routes>
         
         <div>
           <div className="row">
