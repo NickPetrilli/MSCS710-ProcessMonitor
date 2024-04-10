c43c3c67f19fa5757fe2b0c4777804e7475d7dac
Ben Fiore <benjfiore15@gmail.com>
2024-04-10 14:47:47 -0400

    Some UI Cleanup Of The Disk Glance View 4.10.24

--- sysmon-plus/src/Pages/Glance/DiskGlance.js
+++ sysmon-plus/src/Pages/Glance/DiskGlance.js
@@ -7,8 +7,12 @@ const DiskSection = () => {
   const [selectedDiskName, setSelectedDiskName] = useState('');
   const [topProcesses, setTopProcesses] = useState([]);
 
-  const divStyle = {
-    marginTop: '3%'
+  const titleStyle = {
+    marginTop: '2%'
+  };
+
+  const utilStyle = {
+    marginRight: '20%'
   };
 
   useEffect(() => {
@@ -60,24 +64,27 @@ const DiskSection = () => {
 
   return (
     <div className="section-Disk">
-      <Link to={{
-            pathname: "/disk-detail"
-      }}>
-        <h1 className='section-title'>Disk</h1>
-      </Link>
-      <h4 className='section-title' style={divStyle}>{disks.length > 0 && (
-        <select className='disk-selection-menu' onChange={handleDiskChange} value={selectedDiskName}>
-          {disks.map((diskItem) => (
-            <option className='disk-selection-option' key={diskItem.name} value={diskItem.name}>{diskItem.model} - {diskItem.name}</option>
-          ))}
-        </select>
-      )} </h4>
+      <div className='section-Disk-TITLE'>
+        <Link to={{
+              pathname: "/disk-detail"
+        }}>
+          <h1>Disk</h1>
+        </Link>
+
+        <h4>{disks.length > 0 && (
+          <select className='disk-selection-menu' onChange={handleDiskChange} value={selectedDiskName}>
+            {disks.map((diskItem) => (
+              <option className='disk-selection-option' key={diskItem.name} value={diskItem.name}>{diskItem.model} - {diskItem.name}</option>
+            ))}
+          </select>
+        )} </h4>
+      </div>
 
       <div className="glance-row-disk">
         {selectedDiskName && <DiskLineChartFromAPI key={selectedDiskName} diskName={selectedDiskName} view='glance' />}
         {!selectedDiskName && <p>Select a disk to view its details.</p>}
 
-        <div className="utilandTopProc-sec">
+        <div className="utilandTopProc-sec" style={utilStyle}>
           <div className="row">
             <Link to={{
                   pathname: "/disk-detail"
