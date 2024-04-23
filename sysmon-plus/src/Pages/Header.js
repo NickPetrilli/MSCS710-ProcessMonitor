760a1402260ad8564d8b02316ffe87cf0f2366eb
Ben Fiore <benjfiore15@gmail.com>
2024-04-23 18:53:15 -0400

    Added formatting to "Added To Reports" Link In Header 4.23.24

--- sysmon-plus/src/Pages/Header.js
+++ sysmon-plus/src/Pages/Header.js
@@ -1,11 +1,16 @@
 import logo from '../Images/SysMon+ Logo Dark.png';
 import { Link } from 'react-router-dom';
 
+const reportLinkStyle = {
+        margin: '2%'
+};
+
+
 const Header = () => {
     return (
         <header className="App-header">
         <Link to="/"> <img src={logo} alt="" className="App-logo-header" /> </Link>
-        <Link to="/reports"> Click for Reports </Link>
+        <Link to="/reports" style={reportLinkStyle}> Click for Reports </Link>
         </header>
     )
 };
