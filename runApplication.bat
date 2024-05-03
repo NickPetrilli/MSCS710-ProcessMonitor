5e848a24a278f31b3a8a6d4e3e1feedff0021d04
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-05-03 11:40:49 -0400

    Updated batch files for production build
    Batch files now successfully open the web page without the user having to manually enter the command

--- runApplication.bat
+++ runApplication.bat
@@ -1,9 +1,11 @@
 @echo off
 title Run Application
 
-cd ProcessMonitor\process-monitor\target
-start cmd /k java -jar process-monitor-0.0.1-SNAPSHOT.jar
-cd ..\..\sysmon-plus
-start cmd /k npm install -g serve & serve -s build
+start cmd /k java -jar process-monitor-1.0.0.jar
+start cmd /k call npm-install-and-serve.bat
+
+REM Wait for the previous command to finish executing
+timeout /t 5 /nobreak
+
 start http://localhost:3000
 
